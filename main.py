ff3b0f525419181520b710d209c47d0cfa63b45b
Zubair ZFI. Farahi <zfarahi@tc-s.eu>
2024-06-06 13:12:50 +0200

    next

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import torch
 from models import GPTModel, create_dataloader_v1
-from utils import get_tokenizer, generate_text_simple, token_ids_to_text, text_to_token_ids, calc_loss_batch, calc_loss_loader
+from utils import get_tokenizer, generate_text_simple, token_ids_to_text, text_to_token_ids,generate, calc_loss_batch, calc_loss_loader
 from config import load_config
 import argparse
 from train import train_model_simple
@@ -24,6 +24,7 @@ def plot_losses(epochs_seen, tokens_seen, train_losses, val_losses):
 
 
 def train(args):
+
     GPT_CONFIG_124M = load_config(args.config_file)
     print("Loaded configuration:", GPT_CONFIG_124M)
     tokenizer = get_tokenizer("gpt2")
@@ -80,7 +81,116 @@ def train(args):
     epochs_tensor = torch.linspace(0, num_epochs, len(train_losses))
     plot_losses(epochs_tensor, track_tokens_seen, train_losses, val_losses)
 
-def main(args):
+def assign(left, right):
+    if left.shape != right.shape:
+        raise ValueError(f"Shape mismatch. Left: {left.shape}, "
+                          "Right: {right.shape}"
+        )
+    return torch.nn.Parameter(torch.tensor(right))
+
+import numpy as np
+ 
+def load_weights_into_gpt(gpt, params):
+    gpt.pos_emb.weight = assign(gpt.pos_emb.weight, params['wpe'])
+    gpt.tok_emb.weight = assign(gpt.tok_emb.weight, params['wte'])
+    
+    for b in range(len(params["blocks"])):
+        q_w, k_w, v_w = np.split(
+            (params["blocks"][b]["attn"]["c_attn"])["w"], 3, axis=-1)
+        gpt.trf_blocks[b].att.W_query.weight = assign(
+            gpt.trf_blocks[b].att.W_query.weight, q_w.T)
+        gpt.trf_blocks[b].att.W_key.weight = assign(
+            gpt.trf_blocks[b].att.W_key.weight, k_w.T)
+        gpt.trf_blocks[b].att.W_value.weight = assign(
+            gpt.trf_blocks[b].att.W_value.weight, v_w.T)
+ 
+        q_b, k_b, v_b = np.split(
+            (params["blocks"][b]["attn"]["c_attn"])["b"], 3, axis=-1)
+        gpt.trf_blocks[b].att.W_query.bias = assign(
+            gpt.trf_blocks[b].att.W_query.bias, q_b)
+        gpt.trf_blocks[b].att.W_key.bias = assign(
+            gpt.trf_blocks[b].att.W_key.bias, k_b)
+        gpt.trf_blocks[b].att.W_value.bias = assign(
+            gpt.trf_blocks[b].att.W_value.bias, v_b)
+ 
+        gpt.trf_blocks[b].att.out_proj.weight = assign(
+            gpt.trf_blocks[b].att.out_proj.weight, 
+            params["blocks"][b]["attn"]["c_proj"]["w"].T)
+        gpt.trf_blocks[b].att.out_proj.bias = assign(
+            gpt.trf_blocks[b].att.out_proj.bias, 
+            params["blocks"][b]["attn"]["c_proj"]["b"])
+ 
+        gpt.trf_blocks[b].ff.layers[0].weight = assign(
+            gpt.trf_blocks[b].ff.layers[0].weight, 
+            params["blocks"][b]["mlp"]["c_fc"]["w"].T)
+        gpt.trf_blocks[b].ff.layers[0].bias = assign(
+            gpt.trf_blocks[b].ff.layers[0].bias, 
+            params["blocks"][b]["mlp"]["c_fc"]["b"])
+        gpt.trf_blocks[b].ff.layers[2].weight = assign(
+            gpt.trf_blocks[b].ff.layers[2].weight, 
+            params["blocks"][b]["mlp"]["c_proj"]["w"].T)
+        gpt.trf_blocks[b].ff.layers[2].bias = assign(
+            gpt.trf_blocks[b].ff.layers[2].bias, 
+            params["blocks"][b]["mlp"]["c_proj"]["b"])
+ 
+        gpt.trf_blocks[b].norm1.scale = assign(
+            gpt.trf_blocks[b].norm1.scale, 
+            params["blocks"][b]["ln_1"]["g"])
+        gpt.trf_blocks[b].norm1.shift = assign(
+            gpt.trf_blocks[b].norm1.shift, 
+            params["blocks"][b]["ln_1"]["b"])
+        gpt.trf_blocks[b].norm2.scale = assign(
+            gpt.trf_blocks[b].norm2.scale, 
+            params["blocks"][b]["ln_2"]["g"])
+        gpt.trf_blocks[b].norm2.shift = assign(
+            gpt.trf_blocks[b].norm2.shift, 
+            params["blocks"][b]["ln_2"]["b"])
+ 
+    gpt.final_norm.scale = assign(gpt.final_norm.scale, params["g"])
+    gpt.final_norm.shift = assign(gpt.final_norm.shift, params["b"])
+    gpt.out_head.weight = assign(gpt.out_head.weight, params["wte"])
+    
+def prtrain(args):
+    print("Settings:", settings)
+    print("Parameter dictionary keys:", params.keys())
+    # print(params["wte"])
+    
+    model_configs = {
+        "gpt2-small (124M)": {"emb_dim": 768, "n_layers": 12, "n_heads": 12},
+        "gpt2-medium (355M)": {"emb_dim": 1024, "n_layers": 24, "n_heads": 16},
+        "gpt2-large (774M)": {"emb_dim": 1280, "n_layers": 36, "n_heads": 20},
+        "gpt2-xl (1558M)": {"emb_dim": 1600, "n_layers": 48, "n_heads": 25},
+    }
+    GPT_CONFIG_124M = load_config(args.config_file)
+    print("Loaded configuration:", GPT_CONFIG_124M)
+    model_name = "gpt2-small (124M)"
+    NEW_CONFIG = GPT_CONFIG_124M.copy()
+    NEW_CONFIG.update(model_configs[model_name])
+    NEW_CONFIG.update({"context_length": 1024})
+    NEW_CONFIG.update({"qkv_bias": True})
+    gpt = GPTModel(NEW_CONFIG)
+    gpt.eval()
+    
+    load_weights_into_gpt(gpt, params)
+    
+    
+    tokenizer = get_tokenizer("gpt2")
+    start_context = "Every effort moves you"
+   
+    torch.manual_seed(123)
+    token_ids = generate(
+        model=gpt,
+        idx=text_to_token_ids(tokenizer, start_context), 
+        max_new_tokens=25,
+        context_size=NEW_CONFIG["context_length"],
+        top_k=50,
+        temperature=1.5
+    )
+    print("Output text:\n", token_ids_to_text(token_ids, tokenizer))
+    
+    
+def main(args):    
+    print("Token embedding weight tensor dimensions:", params["wte"].shape)
     GPT_CONFIG_124M = load_config(args.config_file)
     print("Loaded configuration:", GPT_CONFIG_124M)
     
@@ -136,5 +246,6 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Run GPT model with custom config")
     parser.add_argument('--config_file', type=str, help='Path to the configuration INI file', required=True)
     args = parser.parse_args()
-    main(args)
+    prtrain(args)
+    # main(args)
     # train(args)
--- utils/tokenizer.py
+++ utils/tokenizer.py
@@ -31,3 +31,29 @@ def generate_text_simple(model, idx, max_new_tokens, context_size):
         idx_next = torch.argmax(probas, dim=-1, keepdim=True)
         idx = torch.cat((idx, idx_next), dim=1)
     return idx
+
+def generate(model, idx, max_new_tokens,
+            context_size, temperature=0.0, top_k=None):
+    for _ in range(max_new_tokens):
+        idx_cond = idx[:, -context_size:]
+        with torch.no_grad():
+            logits = model(idx_cond)
+        logits = logits[:, -1, :]
+        if top_k is not None:
+            top_logits, _ = torch.topk(logits, top_k)
+            min_val = top_logits[:, -1]
+            logits = torch.where(
+                logits < min_val,
+                torch.tensor(float('-inf')).to(logits.device),
+                logits
+            )
+        if temperature > 0.0:
+            logits = logits / temperature
+            probs = torch.softmax(logits, dim=-1)
+            idx_next = torch.multinomial(probs, num_samples=1)
+        else:
+            idx_next = torch.argmax(logits, dim=-1, keepdim=True)
+        if idx_next == "eos_id":
+            break
+        idx = torch.cat((idx, idx_next), dim=1)
+    return idx
